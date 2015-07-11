79bb874447ac8e26267e4e2043e8780e00b3c6d6
Shamail Ahmed Siddiqui <shamail.siddiqui.13@ucl.ac.uk>
2015-07-11 03:14:35 +0100

    Added Basic Assumptions Calculator

--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -2,26 +2,59 @@
  * Created by Shamail on 7/8/2015.
  */
 
-    function getData (PMT,SALE){
+function GetBasicAssumptions(Weeks,WeekRental,Sale,BasicData)
+{
+    //Basic data contains information about Investment,loan,period etc
 
-    //no idea about this
-}
+    //PMT should be accurate to decimal points.
+    var investment = BasicData.inv;
+    var equityAmount = investment*BasicData.equityPerc;
+    var debt = investment - equityAmount;
+    var loanPeriod = BasicData.period;
+    var interest = BasicData.rate;
 
-var dataProvider = [
+    var PMT = pmt(interest/12,loanPeriod,debt);
+    var totalLoanRepay  = PMT*loanPeriod;
+
+    var relief = 0;
+    if(BasicData.relief == "SEIS")
     {
-        "category": "Year 1",
-        "rentsale": 1,
-        "lease": 3,
-        "netcash" : 3-1
-    },
-    {
-        "category": "Year 2",
-        "column-1": 6,
-        "column-2": 7
-    },
-    {
-        "category": "Year 3",
-        "column-1": 2,
-        "column-2": 3
+        relief = investment*0.5;
     }
-];
+    else if(BasicData.relief == "EIS")
+    {
+        relief = investment*0.3;
+    }
+
+    var rent = WeekRental*Weeks;
+    var yearLease = PMT*12;
+
+    var data = {
+        "year1" :{
+            "rentSale" : rent + 0,
+            "rentSaleRelief" : rent + 0 + relief,
+            "Lease" : yearLease,
+            "NetCashFlow" : rent - yearLease,
+            "NetCashFlowSEIS" : rent - yearLease + relief
+        },
+        "year2" :{
+            "rentSale" : rent + 0,
+            "rentSaleRelief" : rent + 0,
+            "Lease" : yearLease,
+            "NetCashFlow" : rent - yearLease,
+            "NetCashFlowSEIS" : rent - yearLease
+        },
+        "year3" :{
+            "rentSale" : rent + Sale,
+            "rentSaleRelief" : rent + Sale ,
+            "Lease" : totalLoanRepay - PMT*24,
+            "NetCashFlow" : rent + Sale - (totalLoanRepay - PMT*24),
+            "NetCashFlowSEIS" : rent + Sale - (totalLoanRepay - PMT*24)
+        }
+    }
+
+
+
+
+
+}
